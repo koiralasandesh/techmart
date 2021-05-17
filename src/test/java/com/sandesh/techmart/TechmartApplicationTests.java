a49015be4980822c154e236b3d9f5645cebbad9a
Sandesh Koirala <sandesh.koirala@hotmail.com>
2021-05-16 19:32:10 -0500

    init empty springboot applications

---
+++ src/test/java/com/sandesh/techmart/TechmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandesh.techmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
